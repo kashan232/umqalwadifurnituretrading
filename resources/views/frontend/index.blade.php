b723fbd656ce5471ec6285f30113d0ba19f7ecdd
Kashan Shaikh <shaikhkashan670@gmail.com>
2026-09-04 17:39:47 +0500

    s

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -959,7 +959,7 @@
         @foreach($banners as $key=>$banner)
         <div class="carousel-item {{(($key==0)? 'active' : '')}}">
             <img class="first-slide" src="{{$banner->photo}}" alt="First slide">
-            <div class="carousel-caption d-none d-md-block text-left">
+            <div class="carousel-caption  text-left">
                 <h1 class="wow fadeInDown">{{$banner->title}}</h1>
                 <p>{!! html_entity_decode($banner->description) !!}</p>
                 <a class="btn btn-lg ws-btn wow fadeInUpBig" href="{{route('product-grids')}}" role="button">Shop Now<i class="far fa-arrow-alt-circle-right"></i></i></a>
@@ -4774,6 +4774,7 @@
 </script>
 
 @endpush
+
 
 
 
